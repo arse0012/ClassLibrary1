99b4864a0ef476dc56e6900bacfbb830ab0645e1
Arsen Niyonzima <arse0012@edu.easj.dk>
2020-09-30 13:30:57 +0200

    Creating Bike class and testing it.

---
+++ ClassLibrary1/Bike.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Bike
+    {
+        private int _id;
+        private int _price;
+        private int _gear;
+        private string _color;
+
+        public Bike()
+        {
+
+        }
+
+        public Bike(int id, int price, int gear, string color)
+        {
+            _id = id;
+            _price = price;
+            _gear = gear;
+            _color = color;
+        }
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (value >= 0) _id = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+        public string Color
+        {
+            get => _color;
+            set
+            {
+                if (value != null) _color = value;
+                else throw new ArgumentNullException();
+            }
+        }
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value >= 0) _price = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+        public int Gear
+        {
+            get => _gear;
+            set
+            {
+                if (value >= 3 && value <= 32) _gear = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+
+
+    }
+}
---
+++ ClassLibrary1Tests/BikeTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1.Tests
+{
+    [TestClass()]
+    public class BikeTests
+    {
+        private readonly Bike _bike = new Bike();
+        [TestMethod()]
+        public void IdTest()
+        {
+            _bike.Id = 4;
+            Assert.AreEqual(4,_bike.Id);
+
+            _bike.Id = 0;
+            Assert.AreEqual(0,_bike.Id);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _bike.Id = -1);
+        }
+
+        [TestMethod()]
+        public void ColorTest()
+        {
+            _bike.Color = "Red";
+            Assert.AreEqual("Red", _bike.Color);
+
+            _bike.Color = "Yellow";
+            Assert.AreEqual("Yellow",_bike.Color);
+
+            Assert.ThrowsException<ArgumentNullException>(() => _bike.Color = null);
+        }
+
+        [TestMethod]
+        public void PriceTest()
+        {
+            _bike.Price = 60;
+            Assert.AreEqual(60, _bike.Price);
+
+            _bike.Price = 0;
+            Assert.AreEqual(0,_bike.Price);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _bike.Price = -1);
+        }
+
+        [TestMethod]
+        public void GearTest()
+        {
+            _bike.Gear = 4;
+            Assert.AreEqual(4,_bike.Gear);
+
+            _bike.Gear = 30;
+            Assert.AreEqual(30,_bike.Gear);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _bike.Gear = 2);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _bike.Gear = 33);
+        }
+    }
+}
